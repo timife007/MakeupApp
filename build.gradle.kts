a0fd020af8184dab8ae955f90ae6407a8cfac19e
Shopinverse <timife007@gmail.com>
2022-11-05 13:02:09 -0700

    kts

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript{
+    dependencies{
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.1")
+    }
+}
+plugins {
+    id ("com.android.application") version "7.2.1" apply false
+    id ("com.android.library") version "7.2.1" apply false
+    id ("org.jetbrains.kotlin.android") version "1.6.10" apply false
+    id ("com.google.dagger.hilt.android") version "2.41" apply false
+    id ("com.android.test") version "7.2.1" apply false
+}
+
+//task clean(type: Delete) {
+//    delete = rootProject.buildDir
+//}
+
+tasks.register(name = "type", type = Delete::class) {
+    delete(rootProject.buildDir)
+}
